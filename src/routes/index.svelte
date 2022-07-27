d9e5548ab4dd3fbc432b5a229e1257446e9716b7
Zi Gang <tanzigang93@gmail.com>
2022-07-27 11:38:32 +0800

    docs(index): add simple example code at home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,18 @@
   const info: TSiteInfo = {
     name: 'svelte',
   }
+
+  const handleClick = () => {
+    info.name = 'new site name'
+  }
+
+  $: {
+    console.log(`site name: ${info.name}`)
+  }
 </script>
 
 <h1>Welcome to {info.name}</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev </a>to read the documentation
+  <button on:click={handleClick}>change site name</button>
 </p>
