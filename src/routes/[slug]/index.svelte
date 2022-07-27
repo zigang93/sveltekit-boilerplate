3ec265d511028ffb50d7edbe957220b117c7ca8c
Zi Gang <tanzigang93@gmail.com>
2022-07-27 13:45:48 +0800

    build: add scss and tailwindcss

---
+++ src/routes/[slug]/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+  // depand on folder name ==> [slug]
+  const { slug } = $page.params
+  const { searchParams } = $page.url
+</script>
+
+<h1>hello, dynamic page with params: {slug}, query: {searchParams.toString()}</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,8 @@
     name: 'svelte',
   }
 
-  const handleClick = () => {
-    info.name = 'new site name'
+  const handleClick = (name = 'new site name') => {
+    info.name = name
   }
 
   $: {
@@ -16,8 +16,32 @@
   }
 </script>
 
-<h1>Welcome to {info.name}</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev </a>to read the documentation
-  <button on:click={handleClick}>change site name</button>
-</p>
+<div class="wrapper">
+  <h1>Welcome to {info.name}</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev </a>to read the documentation
+  </p>
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    on:click={() => handleClick()}>change site name</button
+  >
+  <button class="btn btn-blue" on:click={() => handleClick('svelte')}>change back name</button>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    h1 {
+      color: salmon;
+    }
+  }
+
+  .btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
