2c42125a2eda1b99e38c08fa61f6b011d4fae3b5
Zi Gang <tanzigang93@gmail.com>
2022-07-27 11:28:46 +0800

    fix(eslint): fix eslint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,14 @@
-<script>
-  let name = 'hi'
+<script lang="ts">
+  type TSiteInfo = {
+    name: string
+  }
+
+  const info: TSiteInfo = {
+    name: 'svelte',
+  }
 </script>
 
-<h1>Welcome to {name}</h1>
+<h1>Welcome to {info.name}</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev </a>to read the documentation
 </p>
